6408de5224644aadf4429aa3366730344443a07d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-28 14:53:34 +0200

    Add example to doc

--- Result.pm
+++ Result.pm
@@ -126,6 +126,46 @@ Returns list of structures with key => value pairs.
          From Class::Utils::set_params():
                  Unknown parameter '%s'.
 
+=head1 EXAMPLE
+
+=for comment filename=process_result.pl
+
+ use strict;
+ use warnings;
+
+ use WQS::SPARQL::Result;
+
+ my $result_hr = {
+         'head' => {
+                 'vars' => ['item'],
+         },
+         'results' => {
+                 'bindings' => [{
+                         'item' => {
+                                 'type' => 'uri',
+                                 'value' => 'http://www.wikidata.org/entity/Q27954834',
+                         },
+                 }],
+         },
+ };
+
+ my $obj = WQS::SPARQL::Result->new;
+ my @ret = $obj->result($result_hr, ['item']);
+
+ # Dump out.
+ foreach my $ret_hr (@ret) {
+         print "{\n";
+         foreach my $key (keys %{$ret_hr}) {
+                 print "  $key => ".$ret_hr->{$key}.",\n";
+         }
+         print "},\n";
+ }
+
+ # Output:
+ # {
+ #   item => Q27954834,
+ # },
+
 =head1 DEPENDENCIES
 
 L<Class::Utils>,
